7dc88161ed79f3d96aee2b7282db5462e355d942
d00055a <danieldedja2@gmail.com>
2025-09-12 23:49:41 +0200

    Update Dashboard.jsx

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -34,6 +34,15 @@ export default function Dashboard({ user, setUser }) {
       
       <p className='secretMsg'>{secret}</p>
 
+      {error && (
+        <div>
+
+          <p className="errorStyle">{error}</p>
+
+        </div>
+
+      )}
+
     </div>
 
   );
