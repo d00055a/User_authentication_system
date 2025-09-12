691bcbb6e5d5fe1fa956c7b7feafc4e9e379fea5
d00055a <danieldedja2@gmail.com>
2025-09-12 19:32:33 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from 'react';
 import AuthForm from './AuthForm';
 import Dashboard from './Dashboard';
@@ -34,37 +33,4 @@ export default function App() {
 
   );
 }
-=======
-import { useState } from 'react';
-import AuthForm from './AuthForm';
-import Dashboard from './Dashboard';
-import './App.css';
 
-export default function App() {
-  const [user, setUser] = useState(null);
-
-  return (
-
-    <div className='app'> 
-
-     <header className='app-header'>
-      <h1>User authentication system</h1>
-     </header>
-
-     <section className='content'>
-
-        <p className='description'>Authentication demo</p>
-
-         {user ? <Dashboard user={user} setUser={setUser} /> : <AuthForm setUser={setUser} />}
-
-     </section>
-
-      <footer className="app-footer">
-        <p>© 2025 Daniel Dedja. All rights reserved.</p>
-      </footer>
-
-    </div>
-
-  );
-}
->>>>>>> 5be6ea3fee4677609a468473a29dffcc10e79c92
