179397f19e493a35760c38a5e8e6d4c789f02ce5
d00055a <danieldedja2@gmail.com>
2025-09-12 19:53:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
->>>>>>> 5be6ea3fee4677609a468473a29dffcc10e79c92
